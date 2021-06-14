8ffcd55d42e1d05e712868fc496e0ccf4cce9b80
Abror <70596977+Abror4544@users.noreply.github.com>
2021-06-14 15:39:17 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,6 +17,17 @@ $(function () {
     }
 
     PaddingCalcSection();
+
+
+    $('.almaty').click(function(){
+        $('.mapBlock iframe').attr('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2574.5565625588447!2d73.07826461512755!3d49.813203840956625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x424346d75b67509d%3A0xf94180a1fa0351fb!2zMjAx0J3Qnywg0L_RgNC-0YHQvy4g0JHRg9GF0LDRgC3QltGL0YDQsNGDIDIwLCDQmtCw0YDQsNCz0LDQvdC00LAgMTAwMDAwLCDQmtCw0LfQsNGF0YHRgtCw0L0!5e0!3m2!1sru!2s!4v1623664656056!5m2!1sru!2s');
+    })
+
+    $('.nursultan').click(function(){
+        $('.mapBlock iframe').attr('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d216.08408863203766!2d76.93655245023069!3d43.22481171444929!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38836f23994228ab%3A0x491e26b6dfda807e!2z0YPQu9C40YbQsCDQnNCw0YDQutC-0LLQsCA3NSwg0JDQu9C80LDRgtGLLCDQmtCw0LfQsNGF0YHRgtCw0L0!5e0!3m2!1sru!2s!4v1623664765586!5m2!1sru!2s');
+    })
+
+
     $('.consultOpen').click(() => {
         $('.consult').css('display', 'flex');
     })
@@ -25,11 +36,18 @@ $(function () {
         $('.consult').css('display', 'none');
     })
 
+    $('.consult .close').click(() => {
+        $('.consult').css('display', 'none');
+    })
+
     $('.call_order').click(() => {
         $('.callOrder').css('display', 'flex');
     })
 
     $('.callOrder .closeForm').click(() => {
+        $('.callOrder').css('display', 'none');
+    })
+    $('.callOrder .close').click(() => {
         $('.callOrder').css('display', 'none');
     })
 
